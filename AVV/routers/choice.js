47ee831cd443db931e510aa170f885a64e4bbb2b
KangGyeongHun <46783450+KangGyeongHun@users.noreply.github.com>
2019-12-19 00:45:07 +0900

    Add files via upload

--- AVV/routers/choice.js
+++ AVV/routers/choice.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const router = express.Router();
 const Choices = require("../models").Choice;
+const path = require('path');
 
 router.post('/feed', function(request, response){
     let description = request.body.description;
@@ -17,10 +18,17 @@ router.post('/feed', function(request, response){
     }).catch(function(err){
         console.error(err);
     });
-//작성중 이미지 클릭시 votecnt++
-    if(onclick.image){
-        votecnt++;
-    }
 });
+
+//작성중 이미지 클릭시 votecnt++
+router.post('/voted', function(request, response){
+    console.log('와 되누 섹'); // 이미지 어디있냐 그거를 이제 feed에서 받아올거임음
+    //걍 일단 그러면 ajax 예시만 보여드림ㅇㅋ
+    response.send({result:false});
+})
+
+router.get('/voted',(req,res)=>{
+    res.sendFile(path.join(__dirname , 'testtest.html'));    
+})
 
 module.exports = router;
